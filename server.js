24d2f972ebbb10719d34af99e49580fd5370fedd
KargavkarKaran <karankargavkar@gmail.com>
2025-12-30 11:06:32 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); 
-
 mongoose.connect("mongodb://127.0.0.1:27017/agriculture", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
